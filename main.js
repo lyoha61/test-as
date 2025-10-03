9d4480f072e15b95e1d8a0c649a5bfa94e837602
lyoha61 <nightmessenger703@gmail.com>
2025-10-03 13:24:53 +0300

    Init project

---
+++ main.js
@@ -0,0 +1,40 @@
+const showDealsBtn = document.querySelector('#show-deals');
+const minSumInput = document.querySelector('#min-sum');
+const dealsTable = document.querySelector('#deals-table');
+
+const methods = {
+	CRM_DEALS_LIST: 'crm.deal.list',
+}
+
+function loadDeals() {
+	const minSum = parseFloat(minSumInput.value) || 0;
+	const tbody = dealsTable.querySelector('tbody');
+	tbody.innerHTML = "<tr><td colspan='3'>Загрузка...</td></tr>";
+	BX24.callMethod(
+		methods.CRM_DEALS_LIST,
+		{ select: ["ID", "TITLE", "OPPORTUNITY"] },
+		function(result) {
+			if (result.error()) {
+				tbody.innerHTML = `<tr><td colspan='3'>Ошибка ${result.error()}</td></tr>`;
+				return
+			}
+
+			const deals = result.data().filter(
+				d => parseFloat(d.OPPORTUNITY) >= minSum
+			);
+
+			tbody.innerHTML = "";
+			deals.forEach(d => {
+				tbody.insertAdjacentHTML("beforeend",
+					`<tr>
+						<td>${d.ID}</td>
+						<td>${d.TITLE}</td>
+						<td>${d.OPPORTUNITY}</td>
+					</tr>`
+				)
+			})
+		}
+	)
+}
+
+showDealsBtn.addEventListener("click", loadDeals);
